629d5b785cc77968c23189c026c73b8e9770088b
Mayerson <mayerson18@gmail.com>
2017-07-05 14:04:05 -0400

    slide imagenes

--- js/index.js
+++ js/index.js
@@ -21,3 +21,30 @@ $a.hover( function(e){
   $(this).find("a")[0].click();
   $(this).addClass("current");
 });
+
+$b = $(".function");
+$b.hover( function(e){//mouseenter
+  e.preventDefault();
+  $(this).addClass("current");
+  var i = 0;
+  var x = $(".active").find(".img");
+  var index = parseInt(x.data("index"));
+  var img = x.data("img");
+  i = (index>=img.length) ? 0 : index;
+  animation(x,img,i);
+},function(){//mouseout
+  $(this).removeClass("current")
+});
+
+function animation(x,img,i){
+  var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+  $(x).removeClass("animated fadeOut zoomIn");
+  $(x).addClass("animated fadeOut").one(animationEnd, function() {
+    $(this).attr("src",img[i]);
+  	i++;
+  	x.data("index",i);
+    $(this).removeClass('fadeOut').addClass("zoomIn").one(animationEnd, function() {
+      $(this).removeClass('animated zoomIn');
+    });
+  });
+}
